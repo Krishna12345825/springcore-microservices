9168404c8af4976f9e2d6224ec2737a0d1011150
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-07-23 15:49:12 +0530

    spring rest project updates

--- SpringRestProj06-ConvertingJSONComplexDataIntoObjects/src/main/java/com/nt/rest/EmployeeOperationsController.java
+++ SpringRestProj06-ConvertingJSONComplexDataIntoObjects/src/main/java/com/nt/rest/EmployeeOperationsController.java
@@ -1,7 +1,7 @@
  //EmployeeOperationsController.java
 package com.nt.rest;
 
-import org.springframework.http.HttpStatus;
+import  org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
